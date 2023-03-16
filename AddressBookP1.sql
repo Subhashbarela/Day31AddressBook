5eac499261c42cc03296c2c881caa86f1fae02df
Subhash Barela <barelasubhash16@gmail.com>
2023-03-16 16:51:17 +0530

    display the grop of similar data and count total

--- AddressBookP1.sql
+++ AddressBookP1.sql
@@ -1,18 +1,14 @@
 
 insert into AddressBook(first_name,last_name,address,city,state,zip,phone,email)
-values('rajveer','barela','33th street','chopda','maharashtra','425100','02586-227565','raajveera@gmail.com')
+values('ram','barela','32th street','chopda','maharashtra','425100','02586-227565','raajveera@gmail.com'),
+('rajveer','chouhan','33th street','jalgaon','madhy pradesh','425100','02586-227565','rajeshera@gmail.com'),
+('ashish','barela','33th street','chopda','goa','425100','02554-226565','jamdesra@gmail.com'),
+('abhi','bode','53th street','nageshwar','gujrat','425100','03486-265565','abhaya@gmail.com')
+
+
 
 select * from AddressBook
 
-update AddressBook set last_name ='david' where first_name ='joney'
+select city, count(*) as total_city from AddressBook group by city
 
-update AddressBook set state ='maharashtra' where first_name ='joney'
-
-select * from AddressBook
-
-select first_name from AddressBook where city ='jalna'
-
-select state from AddressBook where first_name ='joney' or last_name ='barela'
-
-
-select * from AddressBook
+select state, count(state) as total_city from AddressBook group by state
