98b4c53d4c00a5496babc40a3092e309a1cf44f7
Subhash Barela <barelasubhash16@gmail.com>
2023-03-16 15:59:27 +0530

    delete Specific row from table using dml command

--- AddressBookP1.sql
+++ AddressBookP1.sql
@@ -1,9 +1,15 @@
 
+insert into AddressBook(first_name,last_name,address,city,state,zip,phone,email)
+values('rajveer','barela','33th street','chopda','maharashtra','425100','02586-227565','raajveera@gmail.com')
 
 select * from AddressBook
 
 update AddressBook set last_name ='david' where first_name ='joney'
 
 update AddressBook set state ='maharashtra' where first_name ='joney'
+
+select * from AddressBook
+
+delete from AddressBook where first_name ='rajveer'
 
 select * from AddressBook
