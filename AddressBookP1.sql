bef06ba4aed80bdf6050612e2dec654774a7c15e
Subhash Barela <barelasubhash16@gmail.com>
2023-03-16 15:45:55 +0530

    insert the data into table

--- AddressBookP1.sql
+++ AddressBookP1.sql
@@ -11,3 +11,8 @@ CREATE TABLE AddressBook (
   phone VARCHAR(20) NOT NULL,
   email VARCHAR(100) NOT NULL
 )
+
+insert into AddressBook(first_name,last_name,address,city,state,zip,phone,email)
+values('joney','bhagul','12th street','jalna','mh','425100','02586-2272903','johnAb@gmail.com')
+
+select * from AddressBook
