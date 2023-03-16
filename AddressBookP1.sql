ae68389a8be8a2635ea4d53cc1514e67cb1f7bd2
Subhash Barela <barelasubhash16@gmail.com>
2023-03-16 15:53:21 +0530

    update the data using update command

--- AddressBookP1.sql
+++ AddressBookP1.sql
@@ -1,18 +1,9 @@
-create database AddressBook
 
-CREATE TABLE AddressBook (
-  id INT identity PRIMARY KEY,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  address VARCHAR(100) NOT NULL,
-  city VARCHAR(50) NOT NULL,
-  state VARCHAR(50) NOT NULL,
-  zip VARCHAR(10) NOT NULL,
-  phone VARCHAR(20) NOT NULL,
-  email VARCHAR(100) NOT NULL
-)
 
-insert into AddressBook(first_name,last_name,address,city,state,zip,phone,email)
-values('joney','bhagul','12th street','jalna','mh','425100','02586-2272903','johnAb@gmail.com')
+select * from AddressBook
+
+update AddressBook set last_name ='david' where first_name ='joney'
+
+update AddressBook set state ='maharashtra' where first_name ='joney'
 
 select * from AddressBook
