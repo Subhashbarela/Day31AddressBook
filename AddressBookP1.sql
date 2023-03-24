586eb375715f55926b5414815747f43e3658168e
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-03-24 16:01:16 +0530

    Update AddressBookP1.sql

--- AddressBookP1.sql
+++ AddressBookP1.sql
@@ -1,13 +1,16 @@
-create database AddressBook
+insert into AddressBook(first_name,last_name,address,city,state,zip,phone,email,name,Contact_type)
+values('ram','barela','32th street','chopda','maharashtra','425100','02586-227565','rambarela@gmail.com','A1','family'),
+('rajveer','chouhan','33th street','jalgaon','madhy pradesh','425100','02586-227565','rajeshera@gmail.com','A2','profession'),
+('ashish','barela','33th street','chopda','goa','425100','02554-226565','ashishb@gmail.com','A3','friends'),
+('abhi','bode','53th street','nageshwar','gujrat','425100','03486-265565','abhaya@gmail.com','A2','family'),
+('naveen','chouhan','53th street','nageshwar','gujrat','456100','03486-265565','naveen@gmail.com','A2','friends')
 
-CREATE TABLE AddressBook (
-  id INT identity PRIMARY KEY,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  address VARCHAR(100) NOT NULL,
-  city VARCHAR(50) NOT NULL,
-  state VARCHAR(50) NOT NULL,
-  zip VARCHAR(10) NOT NULL,
-  phone VARCHAR(20) NOT NULL,
-  email VARCHAR(100) NOT NULL
-)
+
+
+
+select * from AddressBook
+ 
+
+INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone, email, name, contact_type)
+VALUES ('Doctor', 'David', '123 Main St', 'Pune', 'MH', '10001', '5645-55567', 'Doct12@gmail.com', 'A3', 'Friends'),
+       ('Doctor', 'David', '123 Main St', 'Jalna', 'MP', '10001', '0555-5555', 'Doct12@gmail.com', 'A1', 'Family'
